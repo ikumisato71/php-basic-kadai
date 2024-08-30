fb3aa4c64b198cb89f19185c89bfe2fcceeaeb5d
ikumisato71 <kinoko7971@gmail.com>
2024-08-30 18:05:44 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>SCORE PHP</title>
+  </head>
+  <body>
+    <P>
+      <?php
+      $score1= 80;
+      $score2= 60;
+      $score3= 55;
+      $score4= 40;
+      $score5= 100;
+      $score6= 25;
+      $score7= 80;
+      $score8= 95;
+      $score9= 30;
+      $score10= 60;
+      // TOTAL
+      $total_score= $score1+ $score2+ $score3+ $score4+ $score5+ $score6+ $score7+ $score8+ $score9+ $score10;
+      $number_of_scores = 10;
+      $average_score = $total_score / $number_of_scores;
+      echo "平均スコアは: {$average_score}";
+      ?>
+    </P>
+  </body>
+</html>
