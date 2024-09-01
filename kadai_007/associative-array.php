dbdee21777fb7db0dd190347cd09b2659665d04c
ikumisato71 <kinoko7971@gmail.com>
2024-09-01 18:21:43 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題007</title>
+  </head>
+
+  <body>
+    <P>
+      <?php
+      $vegetable_data=['name'=>'onion','price'=>200,'weight'=>160];
+      print_r($vegetable_data);
+      ?>
+
+    </P>
+  </body>
+</html>
